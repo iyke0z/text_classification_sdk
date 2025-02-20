0d53beb561f32588b3a3270dc3d63062fbd4aff7
Ikenna Anikwe <31927515+iyke0z@users.noreply.github.com>
2025-02-20 13:46:52 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="text_classifier_sdk",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "torch",
@@ -17,5 +17,6 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/iyke0z/text-classifier-sdk",
-    python_requires=">=3.7",
+    python_requires=">=3.11",
+    license = "MIT",
 )
